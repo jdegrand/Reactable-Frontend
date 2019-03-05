cdd6bfbe93a32fa2d631b65e866d1c7941c32e79
Joe DeGrand <degrandjoe@gmail.com>
2019-03-05 00:32:58 -0500

    Initial React commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from './styledComponents';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <p>Yert</p>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/styledComponents.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    color: green;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
